723e77191c61484f9d32ef26251af50f0e940342
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-05-10 11:30:11 +0500

    TasksReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 import TodoForm from "./react-query/TodoForm";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
   return (
     <>
-      <Counter />
+      <TaskList />
     </>
   );
 }
